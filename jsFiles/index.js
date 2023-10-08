6e74f5c9fabd425fe87d4ed2ed361c7d7091d8f8
MelonFruit7 <leogofman23@gmail.com>
2023-10-08 10:26:10 -0400

    Fix for github.io to work

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -3,7 +3,7 @@ let wildSpaceImageCount = 6;
 
 let imageCounter = 1, imageAlternate = 1;
 setInterval(() => {
-    imgElements[imageAlternate%2].src = `../images/wildspace${imageCounter+1}.jpg`
+    imgElements[imageAlternate%2].src = `images/wildspace${imageCounter+1}.jpg`
 
     imgElements[imageAlternate%2].style.opacity = 1;
     imgElements[(imageAlternate+1)%2].style.opacity = 0;
